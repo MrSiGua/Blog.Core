c5debb83be5d036970315997fe8968ed110c637a
SiGua <1058435977@qq.com>
2019-06-25 11:32:33 +0800

    1. 配置Swagger服务。

--- Blog.Core/Startup.cs
+++ Blog.Core/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Blog.Core
 {
@@ -26,6 +27,25 @@ namespace Blog.Core
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Version = "V0.1.0",
+                    Title = "Blog.Core.API",
+                    Description = "框架说明文档",
+                    TermsOfService = "None",
+                    Contact = new Swashbuckle.AspNetCore.Swagger.Contact
+                    {
+                        Name = "Blog.Core",
+                        Email = "Blog.Core@xxx.com",
+                        Url = "https://www.jianshu.com/u/94102b59cc2a"
+                    }
+                });
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +60,15 @@ namespace Blog.Core
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            #region Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiHelp V1");
+                // /v1/必须与上文路径一致。
+            });
+            #endregion
 
             app.UseHttpsRedirection();
             app.UseMvc();
